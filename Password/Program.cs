cb4ab69ce393b26e3d4f1c67295ed55d24f1ea21
ArcadeArchie <archie3@web.de>
2019-10-01 09:14:17 +0200

    Added Filter functionality and began work for the WinForms part

--- Password/Program.cs
+++ Password/Program.cs
@@ -6,7 +6,9 @@ using System.Net;
 using System.Net.Http;
 using System.Security.Cryptography;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Password
 {
@@ -14,8 +16,10 @@ namespace Password
     {
         public static string FiveHash;
         private static string CompleteHash;
+        //Turns the given string into a sha1 hash for the API
         public static string Hash(string input)
         {
+            //Turn the given password into a sha1 hash
             SHA1Managed sha1 = new SHA1Managed();
             var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(input));
             var sb = new StringBuilder(hash.Length * 2);
@@ -24,54 +28,85 @@ namespace Password
             {
                 sb.Append(b.ToString("X2"));
             }
+            // Rest of the hash for filtering the list returned by the api
+            CompleteHash = sb.ToString().Substring(5);
+            //First five characters of the hash that gets send to the API
             FiveHash =
             !String.IsNullOrWhiteSpace(sb.ToString()) && sb.ToString().Length >= 5
             ? sb.ToString().Substring(0, 5)
             : sb.ToString();
-            CompleteHash = sb.ToString().Substring(5, hash.Length - 5);
+
+
             return sb.ToString();
         }
-
-        public static string SendHTTP(string input)
+        //Sends a Http request to the API and returns the a list with found hashes
+        public static async Task<string> SendHTTP(string input, bool fulloutput = false, bool forFile = false)
         {
-            HttpWebRequest client = WebRequest.Create("https://api.pwnedpasswords.com/range/" + input) as HttpWebRequest;
 
-            HttpWebResponse response = (HttpWebResponse)client.GetResponse();
-
-            var encoding = Encoding.UTF8;
-            using (var reader = new System.IO.StreamReader(response.GetResponseStream(), encoding))
+            var client = new HttpClient();
+            //if its for console output
+            if (!forFile)
             {
-                string responseText = reader.ReadToEnd();
-                return responseText;
+                //send the request to the api with the first five characters of the hash
+                var response = await client.GetStringAsync("https://api.pwnedpasswords.com/range/" + input);
+                //replace the line breaks with semicolons
+                response = response.Replace("\r\n", ";");
+                //split the response into entries where the semicolons are
+                var Array = response.Split(';');
+                //Dictinary for the entries Key: Hash, Value: the number of times it has been cracked
+                Dictionary<string, string> FormattedText = new Dictionary<string, string>();
+                foreach (var item in Array)
+                {
+                    //Get the Hash and Number
+                    var temp = item.Split(':');
+                    //Add the hash and number to the dictionary
+                    FormattedText.Add(temp[0], temp[1]);
+                }
+                //if the user only wants the filtered output 
+                if (!fulloutput)
+                {
+                    response = $"\nYour password has been {FormattedText[CompleteHash]} times cracked";
+                }//if the user wants the full output
+                else
+                {
+                    response += $"\nYour password has been {FormattedText[CompleteHash]} times cracked";
+                }
+                return response;
+            }//if its for file output
+            else
+            {
+                var response = await client.GetStringAsync("https://api.pwnedpasswords.com/range/" + input);
+                return response;
             }
 
-
         }
-
-        //public static void WritetoFile(string input)
-        //{
-
-        //    var sb = new StringBuilder(CompleteHash.Length * 2);
-        //    var cuthash =
-        //    !String.IsNullOrWhiteSpace(sb.ToString()) && sb.ToString().Length >= 5
-        //    ? sb.ToString().Substring(4, CompleteHash.Length)
-        //    : sb.ToString();
-        //    var path = Environment.CurrentDirectory+"/foundHashes.txt";
-
-        //    if (!File.Exists(path))
-        //    {
-        //        File.WriteAllText(path,SendHTTP(FiveHash));
-        //    }
-        //    Console.WriteLine("All found hashes have been written to the File 'foundHashes.txt' at : " + path);
-        //    Console.WriteLine("to check if your Password is unsafe use CTRL+F in a text editor and search for : " + cuthash);
-        //}
+        [Obsolete("Your password already gets filtered automatically if you still want to use it then please remember it might get removed in the future")]
+        public static void WriteToFile(string input)
+        {
+            var path = Environment.CurrentDirectory + "/foundHashes.txt";
+            if (!File.Exists(path))
+            {
+                File.WriteAllText(path, input);
+            }
+            Console.WriteLine($"All found hashes have been written to the File 'foundHashes.txt' at : {path}");
+            Console.WriteLine($"to check if your Password has been found use CTRL+F in a text editor and search for : {CompleteHash}");
+        }
 
         static void Main(string[] args)
         {
-            Console.WriteLine(Hash(Console.ReadLine()));
-            Console.WriteLine(FiveHash);
-            Console.WriteLine(SendHTTP(FiveHash));
+            Console.Write("Enter a Password: ");
+            Console.WriteLine("Complete Hash: " + Hash(Console.ReadLine()));
+            Console.WriteLine("Part send to the API: " + FiveHash);
+            Console.WriteLine("Result: \n" + SendHTTP(FiveHash).Result);
             Console.ReadLine();
+
         }
+        /*[STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }*/
     }
 }
