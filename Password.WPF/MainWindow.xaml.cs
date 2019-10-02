cb83f8be6cb515c5c54d37aaffd59afd782e6eb2
ArcadeArchie <archie3@web.de>
2019-10-02 13:31:51 +0200

    Added the WPF Version of the Tester

---
+++ Password.WPF/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Password.Core;
+
+namespace Password.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private PwndTester Tester;
+        public MainWindow()
+        {
+            Tester = new PwndTester();
+            InitializeComponent();
+        }
+
+        private void PasswordInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            PasswordHash.Text = Tester.Hash(PasswordInput.Text);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var fulloutput = CheckedFullOutput.IsChecked;
+            var savetofile = CheckedSaveToFile.IsChecked;
+            Output.Text = Tester.SendHTTP(Tester.FiveHash,fulloutput.Value,savetofile.Value);
+        }
+    }
+}
--- Password/Program.cs
+++ Password/Program.cs
@@ -2,23 +2,25 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Net;
 using System.Net.Http;
 using System.Security.Cryptography;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
-namespace Password
+namespace Password.Core
 {
-    class Program
+    public class PwndTester
     {
-        public static string FiveHash;
-        private static string CompleteHash;
+        public string FiveHash;
+        private string CompleteHash;
         //Turns the given string into a sha1 hash for the API
-        public static string Hash(string input)
+        public string Hash(string input)
         {
+            if (string.IsNullOrEmpty(input))
+            {
+                return string.Empty;
+            }
             //Turn the given password into a sha1 hash
             SHA1Managed sha1 = new SHA1Managed();
             var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(input));
@@ -40,69 +42,75 @@ namespace Password
             return sb.ToString();
         }
         //Sends a Http request to the API and returns the a list with found hashes
-        public static async Task<string> SendHTTP(string input, bool fulloutput = false, bool forFile = false)
+        public string SendHTTP(string input, bool fulloutput = false, bool forFile = false)
         {
-
-            var client = new HttpClient();
-            //if its for console output
-            if (!forFile)
+            if (!string.IsNullOrEmpty(input))
             {
-                //send the request to the api with the first five characters of the hash
-                var response = await client.GetStringAsync("https://api.pwnedpasswords.com/range/" + input);
-                //replace the line breaks with semicolons
-                response = response.Replace("\r\n", ";");
-                //split the response into entries where the semicolons are
-                var Array = response.Split(';');
-                //Dictinary for the entries Key: Hash, Value: the number of times it has been cracked
-                Dictionary<string, string> FormattedText = new Dictionary<string, string>();
-                foreach (var item in Array)
+                var client = new HttpClient();
+                //if its for console output
+                if (!forFile)
                 {
-                    //Get the Hash and Number
-                    var temp = item.Split(':');
-                    //Add the hash and number to the dictionary
-                    FormattedText.Add(temp[0], temp[1]);
-                }
-                if (FormattedText.ContainsKey(CompleteHash))
-                {
-                    //if the user only wants the filtered output 
-                    if (!fulloutput)
+                    //send the request to the api with the first five characters of the hash
+                    var response = client.GetStringAsync("https://api.pwnedpasswords.com/range/" + input).Result;
+                    //replace the line breaks with semicolons
+                    response = response.Replace("\r\n", ";");
+                    //split the response into entries where the semicolons are
+                    var Array = response.Split(';');
+                    //Dictinary for the entries Key: Hash, Value: the number of times it has been cracked
+                    Dictionary<string, string> FormattedText = new Dictionary<string, string>();
+                    foreach (var item in Array)
                     {
-                        if (Convert.ToInt32(FormattedText[CompleteHash]) > 0)
+                        //Get the Hash and Number
+                        var temp = item.Split(':');
+                        //Add the hash and number to the dictionary
+                        FormattedText.Add(temp[0], temp[1]);
+                    }
+                    if (FormattedText.ContainsKey(CompleteHash))
+                    {
+                        //if the user only wants the filtered output 
+                        if (!fulloutput)
                         {
-                            Console.BackgroundColor = ConsoleColor.Yellow;
-                            Console.ForegroundColor = ConsoleColor.White;
-                        }
-                        if (Convert.ToInt32(FormattedText[CompleteHash]) > 10)
+                            if (Convert.ToInt32(FormattedText[CompleteHash]) > 0)
+                            {
+                                Console.BackgroundColor = ConsoleColor.Yellow;
+                                Console.ForegroundColor = ConsoleColor.White;
+                            }
+                            if (Convert.ToInt32(FormattedText[CompleteHash]) > 10)
+                            {
+                                Console.BackgroundColor = ConsoleColor.DarkRed;
+                                Console.ForegroundColor = ConsoleColor.Yellow;
+                            }
+                            response = $"Your password has been {FormattedText[CompleteHash]} times cracked";
+                        }//if the user wants the full output
+                        else
                         {
-                            Console.BackgroundColor = ConsoleColor.DarkRed;
-                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            response = response.Replace(";", "\r\n");
+                            response += $"\nYour password has been {FormattedText[CompleteHash]} times cracked\n";
                         }
-                        response = $"Your password has been {FormattedText[CompleteHash]} times cracked";
-                    }//if the user wants the full output
+
+                    }
                     else
                     {
-                        response += $"\nYour password has been {FormattedText[CompleteHash]} times cracked";
+                        Console.BackgroundColor = ConsoleColor.Green;
+                        Console.ForegroundColor = ConsoleColor.White;
+                        response = "Your password hasnt been found it the API database";
                     }
-                    
-                }
+                    return response;
+
+                }//if its for file output
                 else
                 {
-                    Console.BackgroundColor = ConsoleColor.Green;
-                    Console.ForegroundColor = ConsoleColor.White;
-                    response = "Your password has not been cracked yet!";
+                    var response = client.GetStringAsync("https://api.pwnedpasswords.com/range/" + input).Result;
+                    return response;
                 }
-                return response;
-                
-            }//if its for file output
+            }
             else
             {
-                var response = await client.GetStringAsync("https://api.pwnedpasswords.com/range/" + input);
-                return response;
+                return "Please enter a password";
             }
-
         }
         [Obsolete("Your password already gets filtered automatically if you still want to use it then please remember it might get removed in the future")]
-        public static void WriteToFile(string input)
+        public void WriteToFile(string input)
         {
             var path = Environment.CurrentDirectory + "/foundHashes.txt";
             if (!File.Exists(path))
@@ -112,22 +120,5 @@ namespace Password
             Console.WriteLine($"All found hashes have been written to the File 'foundHashes.txt' at : {path}");
             Console.WriteLine($"to check if your Password has been found use CTRL+F in a text editor and search for : {CompleteHash}");
         }
-
-        static void Main(string[] args)
-        {
-            Console.Write("Enter a Password: ");
-            Console.WriteLine("Complete Hash: " + Hash(Console.ReadLine()));
-            Console.WriteLine("Part send to the API: " + FiveHash);
-            Console.WriteLine("Result: \n" + SendHTTP(FiveHash).Result);
-            Console.ReadLine();
-
-        }
-        /*[STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-        }*/
     }
 }
